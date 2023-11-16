94e619c60ea778f921d3316e22fa303ef047454a
Alfie Sun <alfiesunn666@gmail.com>
2023-11-15 22:28:00 -0800

    Edited TetrisGame constructor

--- Tetris/TetrisGame.cpp
+++ Tetris/TetrisGame.cpp
@@ -9,7 +9,7 @@
 	//   load font from file: fonts/RedOctober.ttf
 	//   setup scoreText
 	// - params: already specified
-TetrisGame::TetrisGame(sf::RenderWindow& window, sf::Sprite& blockSprite, const Point& gameboardOffset, const Point& nextShapeOffset) {
+TetrisGame::TetrisGame(sf::RenderWindow& window, sf::Sprite& blockSprite, const Point& gameboardOffset, const Point& nextShapeOffset) : window(window), blockSprite(blockSprite), gameboardOffset(gameboardOffset), nextShapeOffset(nextShapeOffset) {
 	reset();
 	
 	if (!scoreFont.loadFromFile("fonts/RedOctober.ttf")) {
